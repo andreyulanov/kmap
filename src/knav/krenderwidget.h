75b9075a453845848baeb6a9904c1b5b073c70a2
Andrey Ulanov <a.ulanov@mail.ru>
2024-04-01 22:50:46 +0300

    default render_window_size_coef = 2

--- src/knav/krenderwidget.h
+++ src/knav/krenderwidget.h
@@ -24,7 +24,7 @@ public:
     double  pixel_size_mm;
     QSize   window_size;
     int     min_object_size_pix     = 40;
-    double  render_window_size_coef = 1.5;
+    double  render_window_size_coef = 2;
     QColor  background_color        = QColor(166, 220, 238);
     int     update_interval_ms      = 100;
   };
