1eccaa5b0a3bdc119b3846777495f643dc284874
Andrey Ulanov <a.ulanov@mail.ru>
2024-04-11 16:50:04 +0300

    removed places.sitx

--- src/makeworld.cmd
+++ src/makeworld.cmd
@@ -3,7 +3,6 @@ rm /home/user/kmap/data/maps/world.kmap
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class land.json /home/user/kmap/panmaps/land /home/user/kmap/data/maps/global
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class admin0.json /home/user/kmap/panmaps/admin0 /home/user/kmap/data/maps/global
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class admin1.json /home/user/kmap/panmaps/admin1 /home/user/kmap/data/maps/global
-/home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class places.json /home/user/kmap/panmaps/places /home/user/kmap/data/maps/global
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class rivers.json /home/user/kmap/panmaps/rivers /home/user/kmap/data/maps/global
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class lakes.json /home/user/kmap/panmaps/lakes /home/user/kmap/data/maps/global
 /home/user/kmap/build-kmap-Desktop-Debug/pan2kmap/pan2kmap /home/user/kmap/src/class roads.json /home/user/kmap/panmaps/roads /home/user/kmap/data/maps/global
