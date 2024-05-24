7151eaf86816796a90503c989b81a5e7d3865e0a
Andrey Ulanov <a.ulanov@mail.ru>
2024-05-24 22:43:35 +0300

    moved worldbase up

---
+++ makeworld.cmd
@@ -0,0 +1,11 @@
+rm /home/user/ksys/data/packs/global/*
+rm /home/user/ksys/data/packs/world.kpack
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class land.json /home/user/ksys/worldbase/land /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class admin0.json /home/user/ksys/worldbase/admin0 /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class admin1.json /home/user/ksys/worldbase/admin1 /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class rivers.json /home/user/ksys/worldbase/rivers /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class lakes.json /home/user/ksys/worldbase/lakes /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class roads.json /home/user/ksys/worldbase/roads /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class boundaries.json /home/user/ksys/worldbase/boundaries /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class global.json /home/user/ksys/panmaps /home/user/ksys/data/packs/global
+/home/user/ksys/build-ksys-Desktop-Debug/kunite/kunite /home/user/ksys/data/packs/global ru-spe,ru-len.kpack /home/user/ksys/data/packs/world.kpack
