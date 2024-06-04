923286177949e35f50f105650abc15aed95136bc
Andrey Ulanov <a.ulanov@mail.ru>
2024-06-04 17:23:17 +0300

    removed QObject from KRenderPack

--- src/lib/krenderpack.h
+++ src/lib/krenderpack.h
@@ -5,32 +5,26 @@
 
 class KRender;
 
-class KRenderPack: public QObject, public KPack
+struct KRenderPack: public KPack
 {
-  Q_OBJECT
-
   struct RenderAddress
   {
     int layer_idx;
     int obj_idx;
   };
 
-public:
   static constexpr int max_layer_count = 24;
   static constexpr int render_count    = 4;
-
-  KRender*             render = nullptr;
+  KRender*             render          = nullptr;
   QVector<KObject*>    render_data[max_layer_count];
   QReadWriteLock       main_lock;
   QReadWriteLock       tile_lock;
   QList<RenderAddress> render_start_list;
   int                  render_object_count = 0;
 
-  void addCollectionToIndex(KTile& collection);
-
-public:
   KRenderPack(KRender* render, const QString& path);
   ~KRenderPack();
+  void addCollectionToIndex(KTile& collection);
   void clear();
   void loadMain(bool load_objects, double pixel_size_mm);
   void loadTile(int tile_idx);
