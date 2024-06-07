cf33a3e2085f94af5989952d84b4788105333499
Andrey Ulanov <a.ulanov@mail.ru>
2024-06-07 19:03:40 +0300

    removed unneeded land and ocean from world pack

--- makeworld.cmd
+++ makeworld.cmd
@@ -1,6 +1,5 @@
 rm /home/user/ksys/data/packs/global/*
 rm /home/user/ksys/data/packs/world.kpack
-/home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class land.json /home/user/ksys/worldbase/land /home/user/ksys/data/packs/global
 /home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class admin0.json /home/user/ksys/worldbase/admin0 /home/user/ksys/data/packs/global
 /home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class admin1.json /home/user/ksys/worldbase/admin1 /home/user/ksys/data/packs/global
 /home/user/ksys/build-ksys-Desktop-Debug/pan2kpack/pan2kpack /home/user/ksys/src/class rivers.json /home/user/ksys/worldbase/rivers /home/user/ksys/data/packs/global
